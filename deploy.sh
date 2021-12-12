9f5e1e4ecfae9d3a56346d194fb576c8056b06d0
JangasCodingplace <janisgoesser92@gmail.com>
2021-12-12 14:49:23 +0100

    Quickfix on deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ docker stop logstash-service-job-runner
 
 git pull origin master
 
-docker-compose up --build -d job-runner logstash-service-job-runner
+docker-compose up --build -d job-runner logstash-service
