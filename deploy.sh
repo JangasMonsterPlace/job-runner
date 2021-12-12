e1dd906f441d732f622b001ea38a29aa3b960f8e
JangasCodingplace <janisgoesser92@gmail.com>
2021-12-12 14:48:11 +0100

    change container name for logstash

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 docker stop job-runner
+docker stop logstash-service-job-runner
 
 git pull origin master
 
-docker-compose up --build -d job-runner
+docker-compose up --build -d job-runner logstash-service-job-runner
